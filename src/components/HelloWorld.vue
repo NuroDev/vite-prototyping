c44fd1c4792003aa92bcc5a11de1bdcf12fc3b28
N U R O ™ <me@nuro.dev>
2021-01-10 05:20:16 +0000

    Updated HelloWorld component styles

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 v-text="msg" />
 
   <button 
     @click="increment"
-    class="m-8 px-8 py-2 rounded-md text-white bg-blue-500 hover:bg-blue-600 transition ease-in-out duration-300 focus:outline-none focus:ring-2 focus:ring-blue-300"
+    class="m-8 px-8 py-2 rounded-md text-white bg-green-500 hover:bg-green-600 transition ease-in-out duration-300 focus:outline-none focus:ring-4 focus:ring-green-300 focus:ring-opacity-50"
   >
     count is: {{ count }}
   </button>
