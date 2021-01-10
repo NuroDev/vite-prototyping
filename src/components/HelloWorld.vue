959aa8c12f5ae47c23108a72488d8d85bfc134f8
N U R O ™ <me@nuro.dev>
2021-01-10 05:20:00 +0000

    Updated HelloWorld component setup syntax

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,23 +11,14 @@
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
 
-<script>
-import { ref } from 'vue';
+<script setup>
+import { defineProps, ref } from 'vue';
 
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
-  setup() {
-    const count = ref(0);
+const props = defineProps({
+  msg: String,
+});
 
-    const increment = () => count.value++;
+const count = ref(0);
 
-    return {
-      count,
-      increment,
-    }
-  }
-}
+const increment = () => count.value++;
 </script>
