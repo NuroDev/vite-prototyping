2f54070074267d6aaea234da5b3d3985fd4c8c86
N U R O ™ <me@nuro.dev>
2021-01-10 05:22:30 +0000

    Added tailwindcss plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	darkMode: false,
+	future: {
+		defaultLineHeights: true,
+		purgeLayersByDefault: true,
+		removeDeprecatedGapUtilities: true,
+		standardFontWeights: true,
+	},
+	plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+		require('@tailwindcss/typography'),
+	],
+	purge: {
+		enabled: process.env.NODE_ENV === 'production',
+		content: [
+			'./index.html',
+			'./src/**/*.{vue,js,ts,jsx,tsx}',
+		],
+	},
 }
