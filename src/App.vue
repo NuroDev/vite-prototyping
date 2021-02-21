74f38ad7d96c666c44bc1a1e77f18a5aafa0f4a5
nurodev <me@nuro.dev>
2021-02-21 19:45:30 +0000

    Upgraded to Vite 2

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,11 @@
 <template>
-  <img class="mx-auto" alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+	<router-view />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue';
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style lang="postcss">
-/*! @import */
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
-html {
-  @apply antialiased bg-gray-50;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
-
 #app {
-  @apply mt-64 text-center;
-  color: #2c3e50;
+	@apply antialiased text-center mt-64;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	color: #2c3e50;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,58 @@
 <template>
-  <h1 v-text="msg" />
+	<div>
+		<h1>{{ msg }}</h1>
 
-  <button 
-    @click="increment"
-    class="m-8 px-8 py-2 rounded-md text-white bg-green-500 hover:bg-green-600 transition ease-in-out duration-300 focus:outline-none focus:ring-4 focus:ring-green-300 focus:ring-opacity-50"
-  >
-    count is: {{ count }}
-  </button>
+		<p>See <code>README.md</code> for more information.</p>
 
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+		<p>
+			<a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a> |
+			<a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+		</p>
+
+		<button class="button" @click="increment">count is: {{ count }}</button>
+		<p>
+			Edit
+			<code>components/HelloWorld.vue</code> to test hot module replacement.
+		</p>
+	</div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { defineProps, ref } from 'vue';
 
 const props = defineProps({
-  msg: String,
+	msg: {
+		type: String,
+		required: true,
+	},
 });
 
 const count = ref(0);
 
 const increment = () => count.value++;
 </script>
+
+<style scoped lang="postcss">
+a {
+	color: #42b983;
+}
+
+label {
+	margin: 0 0.5em;
+	font-weight: bold;
+}
+
+code {
+	background-color: #eee;
+	padding: 2px 4px;
+	border-radius: 4px;
+	color: #304455;
+}
+
+.button {
+	@apply py-2 px-8 m-8 rounded-md;
+	@apply bg-green-500 hover:bg-green-600 text-white;
+	@apply transition ease-in-out duration-300;
+	@apply focus:outline-none focus:ring-4 focus:ring-green-500 focus:ring-opacity-50;
+}
+</style>
